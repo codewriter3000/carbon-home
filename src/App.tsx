2be3c425b58f34ee724fee057c2b857f80ebb2e7
codewriter3000 <amicharski@outlook.com>
2025-08-23 00:06:17 -0400

    add settings

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import HomeScreen from "./screens/HomeScreen";
 import LockScreen from "./screens/LockScreen";
 import SMS from "./screens/SMS/Conversation";
 import Keyboard from "./components/Keyboard/Keyboard";
+import Settings from "./screens/Settings/Settings";
 import "./App.css";
 
 function App() {
 
   return (
     <main class="container">
-      <SMS />
-			<Keyboard />
+			<Settings />
+      {/*<SMS />
+			<Keyboard />*/}
     </main>
   );
 }
